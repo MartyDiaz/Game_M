138fb397dc2575249e1fb1d786cd9eaa32c93e77
Martin Diaz <martydiaz01@gmail.com>
2020-10-02 21:28:11 -0700

    Test1

---
+++ Main.cpp
@@ -0,0 +1,14 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <string>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[])
+{
+	
+
+	return 0;
+}
