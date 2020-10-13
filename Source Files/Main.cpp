22b35f0273487859dc79ac66880f40e9511b8410
MartyDiaz <67561284+MartyDiaz@users.noreply.github.com>
2020-10-12 17:40:14 -0700

    SPDLOG library added
    SPDLOG from github added. Put it in its own folder and then added that folder to additional includes in visual studio.
    
    Tested and works.

--- Source Files/Main.cpp
+++ Source Files/Main.cpp
@@ -11,11 +11,23 @@
 #include <string>
 #include <iostream>
 
-
+#include "spdlog/spdlog.h"
 
 
 int main(int argc, char* args[])
 {
+
+	spdlog::info("welcome to spdlog!");
+	spdlog::error("Some error message with arg: []", 1);
+
+	spdlog::warn("Easy padding in numbers like {:08d}", 12);
+	spdlog::critical("Support for int: {0:d}; hex: {0:x}; oct: {0:o}; bin: {0:b}", 42);
+	spdlog::info("Positional args are {1} {0}..", "too", "supported");
+
+
+
+
+
 	//*****************************************************************************************
 	// Little test of ECS system
 	/*
